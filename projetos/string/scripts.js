82362b72221a22048e1c633429f04bee0a2b7285
Lucio Costa <luciosdc@outlook.com>
2023-06-27 16:55:26 -0300

    alteração javascript

---
+++ projetos/string/scripts.js
@@ -0,0 +1,9 @@
+console.log(typeof"Oi, meu nome e Lúcio")
+console.log(typeof'Vamos programar')
+console.log(typeof`Olá quanto tempo`)
+console.log(typeof "Inifinity")
+console.log(typeof Infinity)
+
+
+
+
---
+++ projetos/string_avançado/scripts.js
@@ -0,0 +1,10 @@
+console.log("Primeira linha \nSegunda linha");
+console.log("O meu nome é 'Matheus'");
+console.log('O nome dedla e "Roberta"');
+console.log(`A multiplicação de 5 por 3 é ${5*3}`)
+console.log("Isso " + "e " + "uma " + "concatenação")
+
+
+
+
+
