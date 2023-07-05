55f444ab85189351c9e9ede928966d33c5dddfe5
Lucio Costa <luciosdc@outlook.com>
2023-07-05 13:48:45 -0300

    alteração javascript

--- javascript_completo_master/5_EXERCICIOS/8_exercicio_13/scripts.js
+++ javascript_completo_master/5_EXERCICIOS/8_exercicio_13/scripts.js
@@ -1,3 +1,14 @@
+Escreva um loop que exibe números de 100 a 50 no console
+obs.  loop inverso
+
 for(let i = 100; i >= 50; i--) {
   console.log(i);
 }
+
+//declaração de variavel 
+// let num = 100
+//estrutura de repetição for
+for(num = 100; num >= 50; num--){
+  console.log(num)
+}
+
--- javascript_completo_master/5_EXERCICIOS/9_exercicio_14/scripts.js
+++ javascript_completo_master/5_EXERCICIOS/9_exercicio_14/scripts.js
@@ -1,3 +1,6 @@
+//Escreva um loop for ou while que exiba qual número é par e qual número e impar
+//O contador deve iniciar em 0 e ir ate 50
+
 for(let i = 0; i <= 50; i++) {
   
   if(i % 2 == 0) {
@@ -7,3 +10,18 @@ for(let i = 0; i <= 50; i++) {
   }
 
 }
+
+// estrutura de repetição for 
+for(num = 0; num <= 50; num++){
+  //se resto for 0 igual a 0 então par, caso contrario impa
+  if(num % 2 == 0){
+    console.log(num + " e par ")
+    
+  }else
+    console.log(num + " e ímpa ")
+    
+}
+
+/* Verifique se o número é primo
+  Um número primo, é um número natural, maior que 1 e apenas
+  divisível por si próprio e por 1 */
