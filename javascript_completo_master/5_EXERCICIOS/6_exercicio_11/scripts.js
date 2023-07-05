437105003295cb4d519b62318dee4d1867d83692
Lucio Costa <luciosdc@outlook.com>
2023-07-05 10:52:08 -0300

    alteração exercicio

--- javascript_completo_master/5_EXERCICIOS/6_exercicio_11/scripts.js
+++ javascript_completo_master/5_EXERCICIOS/6_exercicio_11/scripts.js
@@ -17,20 +17,46 @@
 // }
 
 
-// EXERCICIO RESOLVIDO POR MIM 
-// declaração de variavel
-let idade = 18
-let cnh = false
+// // EXERCICIO RESOLVIDO POR MIM 
+// // declaração de variavel
+// let idade = 18
+// let cnh = false
 
-// estrutura condicional
-if (idade >= 18 && cnh == true) {
-  console.log("Usuário possui todos requisitos")
-}else if  (idade >= 18 && cnh == false) {
-  console.log("Usuário não possui todos requisitos")
-  // obs. a estrutura esle/if e após o else identificando nenhum valor
-} else { 
-  console.log("Usuário não possui nenhum requisito")
-}
+// // estrutura condicional
+// if (idade >= 18 && cnh == true) {
+//   console.log("Usuário possui todos requisitos")
+// }else if  (idade >= 18 && cnh == false) {
+//   console.log("Usuário não possui todos requisitos")
+//   // obs. a estrutura esle/if e após o else identificando nenhum valor
+// } else { 
+//   console.log("Usuário não possui nenhum requisito")
+// }
 
 // Obs. cuidado na colocação de identificação de das sintaxes de maior > ou menor < 
 // primeiro vem o maior depois os demais sintaxes ex.: <= >=
+
+// Escreva um loop que exibe números de 0 a 10 no console 
+
+// declaração de varivavel num recebe 0
+let num = 0
+//estrutura de repetição while(enquanto)
+// enquanto num for menor ou igual a 10 faça
+while (num <=10){
+// imprimir resultado
+console.log("num é " + num)
+// ao final adiciona mais 1
+num++
+/* 
+num é 0
+num é 1
+num é 2
+num é 3
+num é 4
+num é 5
+num é 6
+num é 7
+num é 8
+num é 9
+num é 10 
+*/
+}
--- javascript_completo_master/5_EXERCICIOS/7_exercicio_12/scripts.js
+++ javascript_completo_master/5_EXERCICIOS/7_exercicio_12/scripts.js
@@ -1,7 +1,34 @@
+// Escreva um loop que exibe números de 0 a 10 no console 
+
+
 let i = 0;
 
 while(i <= 10) {
   console.log(i);
 
   i++;
+}
+
+// declaração de varivavel num recebe 0
+let num = 0
+//estrutura de repetição while(enquanto)
+// enquanto num for menor ou igual a 10 faça
+while (num <=10){
+// imprimir resultado
+console.log("num é " + num)
+// ao final adiciona mais 1
+num++
+/* 
+num é 0
+num é 1
+num é 2
+num é 3
+num é 4
+num é 5
+num é 6
+num é 7
+num é 8
+num é 9
+num é 10 
+*/
 }
