f7f53df524c2a6e9b6c8095984b4f9b2c519287f
Lucio Costa <luciosdc@outlook.com>
2023-07-19 09:37:02 -0300

    atualização
    atualização

--- javascript_completo_master/6_FUNCOES/06_arrow_function/scripts.js
+++ javascript_completo_master/6_FUNCOES/06_arrow_function/scripts.js
@@ -1,3 +1,10 @@
+// Obs. frameworks como angular, react, vue
+/* ARROW FUNCTIONS
+Uma outra forma de escrever funções
+Bem utilizada nos frameworks modernos
+Porém não deve substituir as functions por completo (veremos mais tarde nos detalhes)
+*/
+
 let consoleTeste = () => {
   console.log("Olá mundo!");
 };
