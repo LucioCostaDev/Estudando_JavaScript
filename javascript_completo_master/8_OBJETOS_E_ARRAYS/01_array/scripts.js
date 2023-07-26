fa5ca2984ad46aec43ccb284f46c3958f73a052e
Lucio Costa <luciosdc@outlook.com>
2023-07-26 00:18:35 -0300

    atualização

--- javascript_completo_master/8_OBJETOS_E_ARRAYS/01_array/scripts.js
+++ javascript_completo_master/8_OBJETOS_E_ARRAYS/01_array/scripts.js
@@ -1,10 +1,19 @@
-let arr = [1, 4, 5, 6, 7];
-let nomes = ['Matheus', 'João', 'Aléxia'];
-let bool = [true, false, true];
+/* Arrays
+Possibilidade de adicionar um conjunto de valores a uma variavel
+O array deve ser escrito entre colchetes, separando os valores por virgulas, veja
+*/
 
+// declaração de array com números 
+let arr = [1, 4, 5, 6, 7];
+// declaração de array com string
+let nomes = ['Matheus', 'João', 'Aléxia'];
+// declaração de arrays com booleam
+let bool = [true, false, true];
+// declaração de array diversificado
 let misturado = [1, 'Matheus', true];
 
-console.log(arr);
-console.log(nomes);
-console.log(bool);
-console.log(misturado);
+
+console.log(arr); // [1, 4, 5, 6, 7 ]
+console.log(nomes); // ['Matheus', 'João', 'Aléxia' ]
+console.log(misturado); // [true, false, true]
+console.log(bool); // [1, 'Matheus', true]
