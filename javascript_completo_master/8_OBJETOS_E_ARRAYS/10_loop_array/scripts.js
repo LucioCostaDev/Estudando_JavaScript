05b00d3d14192777e5643e47561f6727e044c171
Lucio Costa <luciosdc@outlook.com>
2023-07-29 00:24:58 -0300

    atualizando
    atualizando conteudo

--- javascript_completo_master/8_OBJETOS_E_ARRAYS/10_loop_array/scripts.js
+++ javascript_completo_master/8_OBJETOS_E_ARRAYS/10_loop_array/scripts.js
@@ -1,5 +1,19 @@
+/* Loop em arrays 
+Uma técnica muito utilizada na programação e o loop nos arrays
+Como é uma lista com muitos valores, muitas vezes precisamos ver cada um deles[
+
+Obs. A lista de array e muita importante ou seja ela serve para interar em cada elemento da array e realizar alguma coisa, por isso utilizar os arrays com loops e uma tecnica bastante utilzadas ex. saber quais produtos estão na promoção num e-comerce, ou seja e recebido pelo back end todos produtos em arrys que e jogado para o front end ou seja pelo javascript com base no array 
+
+obs. tem outas abordagem porem essa e uma padrão
+*/
+
+// declaraçao de arrays
 let nomes = ["Matheus", "Maria", "José"];
 
+// utilizando estrutura de repetição for
+// let recebe a variavel 0; i tem que ser menor ou igual ao comprimento de nome que e 3; i mais pra ir para proximo nome
 for(let i = 0; i <= nomes.length; i++) {
-  console.log(nomes[i]);
+  console.log(nomes[i]); // Matheus Maria José Undefined
 }
+
+//Obs. a variavel [i] vai acessar o indice de cada elementos da array 
--- javascript_completo_master/8_OBJETOS_E_ARRAYS/11_push_pop/scripts.js
+++ javascript_completo_master/8_OBJETOS_E_ARRAYS/11_push_pop/scripts.js
@@ -1,5 +1,14 @@
+/* Métodos de array: push e pop são metodos prontos 
+Push = serve para inserir elementos na array
+Pop = serve para remover um elemento numa array
+
+ambos inserir ou remover sço feitos apatir do fim da array
+*/
+
+// declaração da array com elementos string
 let nomes = ["Matheus", "Maria", "José"];
 
+// 
 let elementoRemovido = nomes.pop();
 
 console.log(elementoRemovido);
