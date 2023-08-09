262fdf324e1f7f1c0ae732f6fae5251a568a40c8
Lucio Costa <luciosdc@outlook.com>
2023-08-09 20:19:38 -0300

    atualização
    atualizando conteudo

--- javascript_completo_master/9_EXERCICIOS/06_exercicio_31/scripts.js
+++ javascript_completo_master/9_EXERCICIOS/06_exercicio_31/scripts.js
@@ -6,19 +6,45 @@ Se tiver mais, imprima "Muitos elementos*/
 
 
 // declaração de arrays  
-let nums = [1,2,3,4,5,6];
-let nums2 = [1,2,3];
+// let nums = [1,2,3,4,5,6];
+// let nums2 = [1,2,3];
 
-//criação da função e (arr) como parâmetro
-function verificaElementosArray(arr) {
-  // estrutura condicional se (parâmetro (arr) .propriedade(.length) for maior ou igual 5
-  if(arr.length >= 5) {
-    console.log("Muitos elementos");
-  } else {
-    console.log("Poucos elementos");
-  }
+// //criação da função e (arr) como parâmetro
+// function verificaElementosArray(arr) {
+//   // estrutura condicional se (parâmetro (arr) .propriedade(.length) for maior ou igual 5
+//   if(arr.length >= 5) {
+//     console.log("Muitos elementos");
+//   } else {
+//     console.log("Poucos elementos");
+//   }
+// }
+
+// // imprimir resultado
+// verificaElementosArray(nums); // Muitos elementos
+// verificaElementosArray(nums2); // Poucos elementos
+
+/* Exercicios 7
+Cria um array com 5 elementos 
+Faça uma interação entre todos eles e imprima no console o valor */
+
+let num = [1,2,3,4,5,];
+
+// utilizando estrutura de repetição for
+// let recebe a variavel 0; i tem que ser menor ou igual ao comprimento de nome que e 3; i mais pra ir para proximo nome
+// for(let i = 0; i <= num.length; i++) {
+  // console.log(num[i]); // Matheus Maria José Undefined
+
+tipodovalor();
+
+
+function tipodovalor() {
+  // console.log(`O valor e um `+ typeof(num));
+
+  num.forEach(element => {
+    console.log(`O valor ` + element + " é um " + typeof(num));
+  });
+  
 }
 
-// imprimir resultado
-verificaElementosArray(nums); // Muitos elementos
-verificaElementosArray(nums2); // Poucos elementos
+
+// obs. O valor é um objeto porque a variável num é um array, e arrays são objetos em JavaScript. A função typeof retorna o tipo de um valor, e no caso, o tipo do valor num é object.
--- javascript_completo_master/9_EXERCICIOS/07_exercicio_32/scripts.js
+++ javascript_completo_master/9_EXERCICIOS/07_exercicio_32/scripts.js
@@ -1,5 +1,40 @@
-let frutas = ["Maçã", "Banana", "Melão", "Melância", "Laranja"];
+/* Exercicios 7
+Cria um array com 5 elementos 
+Faça uma interação entre todos eles e imprima no console o valor */
 
-for(let i = 0; i < frutas.length; i++) {
-  console.log(frutas[i]);
+/* 1º forma resolvida */
+
+// let frutas = ["Maçã", "Banana", "Melão", "Melância", "Laranja"];
+
+// for(let i = 0; i < frutas.length; i++) {
+//   console.log(frutas[i]);
+// }
+
+
+/*  2º forma resolvida */
+// let num = [1,2,3,4,5,];
+
+// // utilizando estrutura de repetição for
+// // let recebe a variavel 0; i tem que ser menor ou igual ao comprimento de nome que e 3; i mais pra ir para proximo nome
+// for(let i = 0; i <= num.length; i++) {
+//   console.log(num[i]); 
+
+// tipodovalor();
+// }
+
+// function tipodovalor() {
+//   console.log(`O valor e um `+ typeof(num));
+// }
+
+/* 3º formade executa */
+let num = [1,2,3,4,5,];
+
+function tipodovalor() {
+  num.forEach(element => {
+    console.log(`O valor ` + element + " é um " + typeof(num));
+  });
 }
+
+tipodovalor();
+
+// obs. O valor é um objeto porque a variável num é um array, e arrays são objetos em JavaScript. A função typeof retorna o tipo de um valor, e no caso, o tipo do valor num é object.
