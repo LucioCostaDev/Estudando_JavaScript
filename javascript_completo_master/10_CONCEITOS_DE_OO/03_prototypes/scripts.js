1538468aa0282a6061653a058cf180f26c3d854e
Lucio Costa <luciosdc@outlook.com>
2023-09-24 21:39:08 -0300

    atualizando conteudo
    javascript

--- javascript_completo_master/10_CONCEITOS_DE_OO/03_prototypes/scripts.js
+++ javascript_completo_master/10_CONCEITOS_DE_OO/03_prototypes/scripts.js
@@ -3,11 +3,12 @@ Um objeto fallback de outro objeto;
 Quando um objeto recebe um requisição de uma propriedade que não tem, ela é procura no prototype deste objeto;
 O protoytpe de um objeto criado do zero é o Object, que tem os métodos nativos da linguagem  */
 
+// declarando objeto pessoa
 const pessoa = {
   maos: 2,
 }
 
-console.log(Object.getPrototypeOf(pessoa));
-console.log(Object.getPrototypeOf(pessoa) === Object.prototype);
+console.log(Object.getPrototypeOf(pessoa)); // Objetct: null prototype~] {}
+console.log(Object.getPrototypeOf(pessoa) === Object.prototype); // true
 
-console.log(pessoa.hasOwnProperty('maos'));
+console.log(pessoa.hasOwnProperty('maos')); // true
--- javascript_completo_master/10_CONCEITOS_DE_OO/04_prototypes_2/scripts.js
+++ javascript_completo_master/10_CONCEITOS_DE_OO/04_prototypes_2/scripts.js
@@ -1,4 +1,10 @@
-const pessoa = {
+/*  Mais sobre o prototypes
+Quando criamos um objeto a partir de um outro, o base será o prototype;
+Ele herdará tanto os métodos e propriedades de Object ( o prototype do objeto base);
+Quanto os do objeto base para este novo; */
+
+// declaração de objeto
+pessoa = {
   maos: 2,
 }
 
@@ -9,8 +15,9 @@ const pessoa = {
 
 const pessoaNova = Object.create(pessoa);
 
-console.log(pessoaNova.maos);
+console.log(pessoaNova.maos); // 2
 
-console.log(pessoaNova.hasOwnProperty('maos'));
+console.log(pessoaNova.hasOwnProperty('maos')); // false
 
-console.log(Object.getPrototypeOf(pessoaNova) === pessoa);
+console.log(Object.getPrototypeOf(pessoaNova) === pessoa); // true 
+
